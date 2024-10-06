d359cc4d2ee570a0c820164ce9132bdd70260687
Vishnucreate <65901404+Vishnucreate@users.noreply.github.com>
2024-10-07 05:21:50 +0530

    Update ObjectDetection.java

--- cloud1/cloud1/src/main/java/com/example/cloud1/ObjectDetection.java
+++ cloud1/cloud1/src/main/java/com/example/cloud1/ObjectDetection.java
@@ -26,6 +26,7 @@ public class ObjectDetection {
     private static final String QUEUE_URL = "https://sqs.us-east-1.amazonaws.com/323052225972/sqsforcarimage";
     private static final Region REGION = Region.of("us-east-1b");
     private static final SdkHttpClient httpClient = ApacheHttpClient.builder().build();
+    @SuppressWarnings("deprecation")
     public static final X509HostnameVerifier ALLOW_ALL_HOSTNAME_VERIFIER = AllowAllHostnameVerifier.INSTANCE;
 
     public static void main(String[] args) {
